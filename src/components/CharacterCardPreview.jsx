d7f798b1806ebf5d9f477bd220ff8d6d1176b2c5
agustin.achille@bairesdev.com <nanacuna1992@gmail.com>
2020-07-25 18:54:25 -0300

    We added close button

--- src/components/CharacterCardPreview.jsx
+++ src/components/CharacterCardPreview.jsx
@@ -2,11 +2,15 @@ import React from 'react';
 import styled from 'styled-components'
 
 const cardStyle = {
-    margin: '5px', 
-    width: '150px',
-    ':hover':{
-        opacity: '.1'
-    }
+    width: '20px',
+    height: '20px',
+    backgroundColor: '#F2C4C0',
+    position: 'relative',
+    top: '25px',
+    cursor: 'pointer',
+    left: '85px',
+    borderRadius: '5px',
+    zIndex: '1'
 };
 
 const Thumbnail = styled.img`
@@ -17,6 +21,7 @@ const Thumbnail = styled.img`
     :hover{
         opacity: 0.7;
     };
+    ${'' /* z-index: 0; */}
 `
 
 export default function CharacterCardPreview({name, thumbnail, onClose}){
@@ -24,9 +29,12 @@ export default function CharacterCardPreview({name, thumbnail, onClose}){
         let image = `${thumbnail.path}/portrait_medium.${thumbnail.extension}`;
     
         return(
-            <div style={cardStyle}>
-                <h3>{name}</h3>
-                <Thumbnail onClick={onClose} src={image} alt=""/>
+            <div>
+                <div style={{backgroundColor:'blue'}} >
+                    <h3 style={{marginBottom:'0px'}}>{name.replace(' ', '\u00a0')}</h3>
+                    <div style={cardStyle} onClick={onClose}>X</div>
+                    <Thumbnail /* onClick={onClose} */ src={image} alt=""/>
+                </div>
             </div>
         );
     }
--- src/components/CharactersCards.jsx
+++ src/components/CharactersCards.jsx
@@ -3,7 +3,7 @@ import CharacterCardPreview from './CharacterCardPreview';
 
 export default function CharactersCards({characters, onClose}){
     return(
-        <div style={{display:'flex'}}>
+        <div style={{display:'flex',  justifyContent:'center'}}>
             {characters && characters.map(character =>
                 <CharacterCardPreview 
                     name={character.name}
